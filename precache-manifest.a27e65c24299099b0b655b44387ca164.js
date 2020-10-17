1ce57fa63838f74f1cd0b12f066c28995eee34cb
Nathan Orlowski <norlowski@gmail.com>
2020-10-17 18:49:03 -0400

    Updates

---
+++ precache-manifest.a27e65c24299099b0b655b44387ca164.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a8f4571bc1def2147a1f4a0a61dca9",
+    "url": "/ChessEnvoy/index.html"
+  },
+  {
+    "revision": "32d039dfc1849e0bc70d",
+    "url": "/ChessEnvoy/static/css/2.a7ba5d00.chunk.css"
+  },
+  {
+    "revision": "15ec3fcb2f8988e5370c",
+    "url": "/ChessEnvoy/static/css/main.2dce71ba.chunk.css"
+  },
+  {
+    "revision": "32d039dfc1849e0bc70d",
+    "url": "/ChessEnvoy/static/js/2.2a46dfe9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ChessEnvoy/static/js/2.2a46dfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ec3fcb2f8988e5370c",
+    "url": "/ChessEnvoy/static/js/main.719b822b.chunk.js"
+  },
+  {
+    "revision": "14c060bcd535980a2502",
+    "url": "/ChessEnvoy/static/js/runtime-main.f35d8f38.js"
+  },
+  {
+    "revision": "7255ba8e48be7967b2eab6482f80d641",
+    "url": "/ChessEnvoy/static/media/Agadmator.7255ba8e.png"
+  },
+  {
+    "revision": "5ed18b9569b9f25934888494fd3ac7ad",
+    "url": "/ChessEnvoy/static/media/Alexandra-Botez.5ed18b95.png"
+  },
+  {
+    "revision": "672b4453b3ba55870a891d10435455b2",
+    "url": "/ChessEnvoy/static/media/ChessNetwork.672b4453.png"
+  },
+  {
+    "revision": "6db6e02b05ef25d2cf4708a39d37558f",
+    "url": "/ChessEnvoy/static/media/Christof-Sielecki.6db6e02b.png"
+  },
+  {
+    "revision": "23abe5cfb2dd1dae3e17afc72d00a758",
+    "url": "/ChessEnvoy/static/media/Eric-Rosen.23abe5cf.png"
+  },
+  {
+    "revision": "64d48b24048c9011c9b2407b704303e1",
+    "url": "/ChessEnvoy/static/media/John-Bartholomew.64d48b24.png"
+  }
+]);
